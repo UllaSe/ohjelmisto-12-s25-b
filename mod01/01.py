1d7bd3bbf3b873e2be5a8f7493a2c23779476011
Ulla Sederlöf <ulla.sederlof@metropolia.fi>
2025-08-19 14:35:38 +0300

    initial commit

---
+++ mod01/01.py
@@ -0,0 +1,13 @@
+# Moikka olen kommentti
+print('Moikka luokka')
+print('Hei "Ulla Sederlöf"')
+print("moi 'Anna'")
+print(8)
+
+# yksinkertainen muuttuja
+nimi = 'Matti'
+print(nimi)
+
+# syöte, eli tietoa käyttäjältä
+nimi2 = input('Moikka, anna nimesi: ')
+print(nimi2)
