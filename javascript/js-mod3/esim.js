45c5c46f66c0ce554cd942db8dc26c5f0a72129a
mattpe <mattpe@metropolia.fi>
2025-11-10 11:38:34 +0200

    add dom & event examples

--- javascript/js-mod3/esim.js
+++ javascript/js-mod3/esim.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // hae viittaus kaikkiin output class elementteihin taulukkona
-const outputElement =
+const outputElements =
     document.getElementsByClassName('output');
-console.log(outputElement);
+console.log(outputElements);
 
 // viittaus yhteen elementtiin id:n perusteella
 const outputElement2 =
@@ -30,9 +30,9 @@ newLi.textContent = 'Uusi itemi';
 
 // haetaan viittaus kaikkiin li-elementteihin listan sisällä
 const liElems = ulElement.querySelectorAll('li');
-for (let i= 0; i<liElems.length; i++) {
+for (let i = 0; i < liElems.length; i++) {
     // muutetaan jokaisen li-elementin sisältöä perustuen taulukon indeksin arvoon
-    liElems[i].textContent = `${i+1}. itemi`;
+    liElems[i].textContent = `${i + 1}. itemi`;
 }
 
 // luodaan html-lista, jossa sisältö js-taulukosta
@@ -49,8 +49,63 @@ const inventoryHeader = document.createElement('h2');
 inventoryHeader.textContent = 'Inventaario';
 body.appendChild(inventoryHeader);
 body.appendChild(inventoryOlElem);
+inventoryHeader.classList.add('red');
 
+// Tapahtumankäsittely
 
+const button = document.querySelector('button');
+// sidotaan tapahtuma ja tapahtumankäsittelijä yhteen
+button.addEventListener('click', buttonClick);
 
+// erillinen tapahtumankäsittelyfunktio
+function buttonClick() {
+    console.log('nappulaa klikattu');
+}
 
+// nimetön funktio tapahtumankäsittelijänä
+inventoryHeader.addEventListener('click', function () {
+    console.log('otsikkoa klikattu');
+    inventoryHeader.classList.toggle('red');
+});
 
+// näppis-tapahtumaesimerkki
+let inputString = '';
+document.addEventListener('keypress', function (event) {
+    //console.log('näppäintä painettu', event);
+    inputString += event.key;
+    if (event.key === 'Enter') {
+        outputElement2.classList.remove('blue');
+        outputElement2.classList.add('red');
+    }
+    //outputElement2.classList.add('blue');
+    outputElement2.textContent = 'nappeja painettu: ' + inputString;
+});
+
+// hiiritapahtuma
+document.addEventListener('mousemove', function (event) {
+    //console.log('hiiri liikkuu', event);
+    outputElements[0].textContent = `Hiiren sijainti: ${event.clientX}, ${event.clientY}`;
+});
+
+// estetään linkin oletustapahtuma
+document.querySelector('a').addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log('linkki klikattu, mutta se ei päivitä sivua');
+});
+
+// lomakkeen käsittely, lisätään asioita aiemmin luotuun inventaarioon
+const addForm = document.querySelector('form');
+const inputText = addForm.querySelector('input');
+addForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    if (inputText.value.length < 3) {
+        return;
+    }
+    const liElem = document.createElement('li');
+    liElem.textContent = inputText.value;
+    // lisätään asia myös js-taulukkoon (ei kuitenkaan käytetä tässä mihinkään)
+    inventory.push(inputText.value);
+    console.log('muistissa oleva inventaario', inventory);
+    inventoryOlElem.appendChild(liElem);
+    inputText.value = '';
+});
